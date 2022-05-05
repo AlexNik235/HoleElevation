078d89982f97a80b57c401e12ba05d432a511483
AlexNik <undead235@gmail.com>
2022-05-06 00:04:45 +0300

    temp

--- HoleElevation/HoleElevation/Cmd.cs
+++ HoleElevation/HoleElevation/Cmd.cs
@@ -1,17 +1,25 @@
 ﻿namespace HoleElevation
 {
+    using Autodesk.Revit.Attributes;
     using Autodesk.Revit.DB;
     using Autodesk.Revit.UI;
+    using ViewModels;
+    using Views;
 
     /// <summary>
     /// Plugin command
     /// </summary>
+    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.Manual)]
     public class Cmd : IExternalCommand
     {
         /// <inheritdoc/>
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            throw new System.NotImplementedException();
+            var mainContext = new MainContext(commandData.Application.ActiveUIDocument);
+            var win = new MainWindow(mainContext);
+            win.ShowDialog();
+            return Result.Succeeded;
         }
     }
 }
